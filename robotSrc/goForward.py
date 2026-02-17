0f298580a09e27bc675829ccd9afc214e57d3d64
Zach-Swartz <164225239+Zach-Swartz@users.noreply.github.com>
2026-02-17 17:00:18 -0500

    update goForward.py with encoders
    update goForward.py with encoders. Encoders to get precise distance that robot travelled.

--- robotSrc/goForward.py
+++ robotSrc/goForward.py
@@ -1,47 +1,57 @@
 from XRPLib.defaults import *
-from XRPLib.rangefinder import Rangefinder
 from time import sleep
 
 def drive():
-    print("Press the button once to start.")
-    board.wait_for_button()
     sleep(0.5)
     
-    rangefinder = Rangefinder.get_default_rangefinder()
-   
-    BASE_SPEED = 30
-    STOP_DIST = 10
+    BASE_SPEED = 20
+    STOP_DIST = 15
     Kp = 0.8
-   
+    # TARGET_COUNTS = 102
+    
     imu.reset_yaw()
-   
+    
     print("Driving...")
-   
+    start_left_count = drivetrain.get_left_encoder_position()
+
     while True:
-        rangeDist = rangefinder.distance()
-        print(f"Range rangeDistance: {rangeDist:.1f} cm")
-       
-        if rangeDist < STOP_DIST:
+        left_count  = drivetrain.get_left_encoder_position()
+        #if start_left_count == left_count:
+        #    break
+        if start_left_count is None:
+            start_left_count = left_count
+        right_count = drivetrain.get_right_encoder_position()
+        
+        left_rotations  = left_count / 288
+        right_rotations = right_count / 288
+
+        print(f"L Count: {left_count:.2f} | R Count: {right_count:.2f}")
+        print(f"L Rotations: {left_rotations:.3f} | R Rotations: {right_rotations:.3f}")
+
+        dist = rangefinder.distance()
+        print(f"Range Distance: {dist:.1f} cm")
+        
+        if dist < STOP_DIST:
             drivetrain.stop()
             print(f"Wall detected < {STOP_DIST} cm. Stopped.")
             break
-       
+        
         current_yaw = imu.get_yaw()
-       
+        
         Y_set = 0.0
         error = Y_set - current_yaw
-       
+        
         correction = error * Kp
-       
+        
         left_speed = BASE_SPEED - correction
         right_speed = BASE_SPEED + correction
-       
+        
         drivetrain.set_speed(left_speed, right_speed)
-       
+        
         print(f"Yaw: {current_yaw:.2f} | Error: {error:.2f} | L: {left_speed:.2f} R: {right_speed:.2f}")
-       
+        
         sleep(0.05)
-   
+    
     drivetrain.stop()
     print("Drive complete.")
 
